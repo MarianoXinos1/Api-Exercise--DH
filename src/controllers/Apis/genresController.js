7bfcd7194135134919b50ac2961e8d4afa9f2a56
MarianoXinos1 <mxinos@hotmail.com>
2024-05-17 16:14:36 -0300

    Add: creacion de Apis: list y detail

--- src/controllers/Apis/genresController.js
+++ src/controllers/Apis/genresController.js
@@ -1,1 +1,25 @@
 const db = require('../database/models');
+
+const genresController = {
+    'list': async function (req,res) {
+
+        try {
+            const genres= await db.Genre.findAll();
+            res.json(genres)
+        } catch (error){
+            console.log('Error al obtener generos Api',error);
+            res.status(500).send('Error al obtener generos Api');
+        }
+    },
+
+    'detail': async function (req,res) {
+        try {
+            const genre = await db.Genre.findByPk(req.params.id);
+            res.json(genre);
+        } catch (error) {
+            console.log('Error al obtener detale Api');
+            res.status(500).send('Error al obtener detalle Api');  // Importante que el send vaya al final porque si va al inicio antes que status no se enviara el status.
+        }
+    }
+
+}
