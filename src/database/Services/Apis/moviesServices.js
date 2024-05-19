c7288bdd361c070bc818e9afe0da4abde201e252
MarianoXinos1 <mxinos@hotmail.com>
2024-05-19 14:55:38 -0300

    Add: Destroy API Service

--- src/database/Services/Apis/moviesServices.js
+++ src/database/Services/Apis/moviesServices.js
@@ -6,7 +6,6 @@ const db = require('../../models');
 const moviesService = {
 
     create: async function(movieData) {
-        
         try {
             return await db.Movie.create(movieData);
         } catch (error) {
@@ -16,17 +15,17 @@ const moviesService = {
     },
 
 
+    destroy: async function(movieId) {
+        try {
+            const movie = await db.Movie.destroy({
+                where: {id: movieId}
+            });
+            return {status: 200}
 
+        } catch (error) {
+            console.log('Error al eliminar la película Service', error);
+        }
+    }
 }
 
 module.exports = moviesService;
-
-
-// destroy: function (req,res) {
-//     let movieId = req.params.id;
-//     Movies
-//     .destroy({where: {id: movieId}, force: true}) // force: true es para asegurar que se ejecute la acción
-//     .then(()=>{
-//         return res.redirect('/movies')})
-//     .catch(error => res.send(error)) 
-// }
