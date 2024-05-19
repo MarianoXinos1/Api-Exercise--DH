4e84dca581640e0ec9283eeac878efdb09210612
MarianoXinos1 <mxinos@hotmail.com>
2024-05-19 15:40:32 -0300

    Add: Destroy API ruta, ya funciona el destroy

--- src/routes/apisMoviesRoutes.js
+++ src/routes/apisMoviesRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const moviesController = require('../controllers/Apis/moviesController');
 
 router.post('/movies', moviesController.create);
+router.delete('/movies/:id', moviesController.destroy)
 
 module.exports = router;
